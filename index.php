85841fe3eb83a0f25b61e47f63fdccca6a881877
Akira <hase.a1995@gmail.com>
2018-03-24 07:24:44 +0000

    Add search screen

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>PostalcodeGet powered by Zipcloud</title>
+    </head>
+    <body>
+        <h1>PostalcodeGet powered by Zipcloud</h1>
+        <form action="search.php" method="get">
+            <input type="number" name="zipcode" placeholder="郵便番号を7桁で入力..."/>
+            <input type="submit" value="検索！"/>
+        </form>
+    </body>
+</html>
